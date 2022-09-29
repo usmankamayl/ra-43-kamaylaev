832313e6d1bb2b0de3ed7453300b4f2ddb2ec44e
usmankamayl <usman.kamailaev@yandex.ru>
2022-09-29 15:54:17 +0300

    func

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import ShopItemFunc from './components/ShopItemFunc';
+import style from './App.css';
+
+function App() {
+  return (
+      <>
+          <div className="container">
+              <div className="background-element">
+                  kkkkkk
+              </div>
+              <div className="highlight-window">
+                  <div className="highlight-overlay">111111</div>
+              </div>
+              <div className="window">
+                  <ShopItemFunc />
+              </div>
+          </div>
+      </>
+
+  );
+}
+
+export default App;
