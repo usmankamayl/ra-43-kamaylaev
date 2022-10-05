d4f9a1c8067907e5accf27add07723d159e668cf
usmankamayl <usman.kamailaev@yandex.ru>
2022-10-05 15:32:41 +0300

    fixed

--- events-state/store/src/App.js
+++ events-state/store/src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-
 import React, { useState } from "react";
-import CardsView from "./components/CardsView";
-import ListView from "./components/ListView";
-import IconSwitch from "./components/IconSwitch";
 import Store from "./components/Store";
 
 function App() {
-  // eslint-disable-next-line no-use-before-define
+
   return (
     <div className="App">
       <Store />
